66f3bb15851cb984ed5eb044b38abc504161ad66
nhAsif <najmulhasan3609@gmail.com>
2026-05-14 12:51:18 +0600

    fix: prevent autorun when no network is available

--- xray/scripts/start.sh
+++ xray/scripts/start.sh
@@ -6,20 +6,16 @@ SCRIPTS_DIR=/data/adb/xray/scripts
 XRAYHELPER=/data/adb/xray/bin/xrayhelper
 XRAYHELPER_CONF=/data/adb/xray/xrayhelper.yml
 
-start_proxy() {
-    ${XRAYHELPER} -c ${XRAYHELPER_CONF} service start &>>/data/adb/xray/run/helper.log &&
-        ${XRAYHELPER} -c ${XRAYHELPER_CONF} proxy enable &>>/data/adb/xray/run/helper.log &
-}
-
 rm -rf /data/adb/xray/run/core.pid
 rm -rf /data/adb/xray/run/adghome.pid
 rm -rf /data/adb/xray/run/tun2socks.pid
 if [ ! -f /data/adb/xray/manual ]; then
     ${XRAYHELPER} >/data/adb/xray/run/helper.log
-    if [ ! -f ${MOD_DIR}/disable ]; then
-        start_proxy
-        sleep 5
-    fi
+    
     inotifyd ${SCRIPTS_DIR}/xray.inotify ${MOD_DIR} &>>/data/adb/xray/run/helper.log &
     inotifyd ${SCRIPTS_DIR}/net.inotify ${NET_DIR} &>>/data/adb/xray/run/helper.log &
+
+    if [ ! -f ${MOD_DIR}/disable ]; then
+        ${SCRIPTS_DIR}/net.inotify w &
+    fi
 fi
